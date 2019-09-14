ecad92ec90451f5f36ba24bbc406431149955cf9
Carlos <miklo.concepcion@gmail.com>
2019-09-13 20:42:14 -0500

    establishing clean communication between app component and each branch. Utilizing semantic ui for sprint design.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import axios from "axios";
+import React from "react-dom";
+import React from "react-router-dom";
+import React from "react-scripts";
+import React from "react-swipeable-views";
+import React from "semantic-ui-css";
+import React from "semantic-ui-css";
+import React from "semantic-ui-react";
+import React from "styled-components";
+
+
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
-
+import SearchForm from "./components/SearchForm";
+import CharacterCard from "./components/CharacterCard";
+import CharacterList from "./components/CharacterList";
+import LocationCard from "./components/LocationCard";
+import LocationList from "./components/LocationList";
+import WelcomePage from "./components/WelcomePage";
+import { Domain } from "domain";
 
 export default function App() {
   return (
